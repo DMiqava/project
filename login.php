9e30376e60ef3fea3ffc29bc4bd5000a3be3c6bc
DMiqava <daviti.mikava.1@btu.edu.ge>
2023-09-14 17:32:10 +0400

    111

--- login.php
+++ login.php
@@ -1,16 +1,42 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Load user data from the JSON file
+    $users = json_decode(file_get_contents("users.json"), true);
+
+    if (isset($users[$username]) && $users[$username] === $password) {
+        // Valid login credentials
+        $_SESSION["username"] = $username;
+        header("Location: dashboard.php");
+        exit;
+    } else {
+        $loginError = "Invalid username or password.";
+    }
+}
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
 <body>
     <h1>Login</h1>
-    <form method="POST" action="authenticate.php">
+    <form method="post">
+        <?php if (isset($loginError)) : ?>
+            <p style="color: red;"><?php echo $loginError; ?></p>
+        <?php endif; ?>
         <label for="username">Username:</label>
-        <input type="text" name="username" required><br><br>
-        
+        <input type="text" id="username" name="username" required><br><br>
+
         <label for="password">Password:</label>
-        <input type="password" name="password" required><br><br>
+        <input type="password" id="password" name="password" required><br><br>
 
         <input type="submit" value="Login">
     </form>
