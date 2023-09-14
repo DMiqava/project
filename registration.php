0c08a1d6e65c53430d390a8f22012737d062b6aa
DMiqava <daviti.mikava.1@btu.edu.ge>
2023-09-14 17:05:00 +0400

    3

--- registration.php
+++ registration.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <h1>Registration</h1>
-    <form method="POST" action="register.php">
+    <form method="POST" action="registration.php">
         <label for="username">Username:</label>
         <input type="text" name="username" required><br><br>
         
@@ -16,3 +16,26 @@
     </form>
 </body>
 </html>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Read the existing users from the JSON file
+    $users = json_decode(file_get_contents("users.json"), true);
+
+    // Check if the username already exists
+    if (array_key_exists($username, $users)) {
+        echo "Username already exists. Please choose a different one.";
+    } else {
+        // Add the new user to the JSON data
+        $users[$username] = $password;
+
+        // Save the updated data back to the JSON file
+        file_put_contents("users.json", json_encode($users));
+
+        echo "Registration successful. <a href='login.php'>Login</a>";
+    }
+}
+?>
